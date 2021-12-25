b7bdd4f38062eedeab257196bfabc1ed24c32751
Michał Lipiec <magunesen@gmail.com>
2021-12-25 16:01:26 +0100

    Co do zrobienia

--- Zaliczenie/Zaliczenie.cpp
+++ Zaliczenie/Zaliczenie.cpp
@@ -1,7 +1,20 @@
-﻿#include <iostream>
+﻿// Napisać program, który utworzy adresową bazę danych(osoba - imię(imiona) i nazwisko(nazwiska),
+// kod, miejscowość, kraj, ulica, numer) i będzie ją obsługiwać(dopisywanie, korekta, kasowanie)
+// STRUCT
+// 
+// oraz sortować według uznania użytkownika algorytmem bąbelkowym.          
+// 
+// Program napisać tak, aby: aby po uruchomieniu była wyświetlana informacja,
+// który raz program został uruchomiony.
+// Stworzyc plik tekstowy z liczba zero, wczytywac ten plik za kazdym razem i dodawac jeden.
+// Wczytaj plik dodaj jeden wyswietl liczbe
+
+
+#include <iostream>
 #include <fstream>
 using namespace std;
+
 int main()
 {
-    cout << "Hello World!\n";
+
 }
